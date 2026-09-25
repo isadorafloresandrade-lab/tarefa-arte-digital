3e24fa42eecab4b99df26702e62fa0818c07048d
isadorafloresandrade-lab <isadora.flores.andrade@escola.pr.gov.br>
2026-09-25 10:38:46 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-conts boesCurtir = document.querySelectorAll(".curtir");
-botoesCurtir.forEach(function(botaoCurtir);
-let curtir = false;
-botaoCurtir.addEventListener("click", curtir);
-functio curtir(){
-    const contador = botaoCurtir.querySelector("span");
-    if(curtiu === false){
-        contador.textContent++;
-        curtiu = true;}
+onst botoesCurtir =document. querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir)
+let curtir=false;
+botaoCurtir.addEventlistener("click", curtir);
+function curtir (){
+    const contador=botaoCurtir.querySelector("span");
+      if(curtiu===false){
+        contador. textContent++;
+        curtiu=true;}
         else{
             contador.textContent--;
-            curtiu = false;
+            curtiu=false;
         }
-    }
-});
+
+
+    
+}
+
